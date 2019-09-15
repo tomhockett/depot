eb9799550858b8d84b7211c8aeee6e78308b1158
Tom <tom@aruxsoftware.com>
2019-09-15 17:18:15 -0500

    init repo and create first resource

---
+++ db/seeds.rb
@@ -0,0 +1,22 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+
+images = %w[7apps.jpg adrpo.jpg dcbang.jpg
+            lorem.jpg rails.png ruby.jpg]
+
+images.each do |img|
+  Product.create!(title: FFaker::Book.title,
+                  description:
+                    %(<p>
+                        <em>#{FFaker::Company.catch_phrase}</em>
+                        #{FFaker::Book.description}
+                    </p>),
+                  image_url: img.to_s,
+                  price: 26.00)
+end
