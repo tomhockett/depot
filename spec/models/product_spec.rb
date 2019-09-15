cf46b5bd44c2ab881f5c8e263a92d65fe5af3a48
Tom <tom@aruxsoftware.com>
2019-09-15 18:58:53 -0500

    first spec - validations

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new(title: "A Title", price: 4.50) }
+
+  context 'validations' do
+    subject { create(:product) }
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:description) }
+    it do
+      is_expected.to validate_numericality_of(:price)
+        .is_greater_than_or_equal_to(0.01)
+    end
+  end
 end
