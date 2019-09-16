c818a8265688eb3e5cc1e131d78b3692c8cee979
Tom <tom@aruxsoftware.com>
2019-09-15 19:04:08 -0500

    add validation test for image_url

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Product, type: :model do
     subject { create(:product) }
     it { is_expected.to validate_presence_of(:title) }
     it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_presence_of(:image_url) }
     it do
       is_expected.to validate_numericality_of(:price)
         .is_greater_than_or_equal_to(0.01)
